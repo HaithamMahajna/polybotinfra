66fa9f0e525ca23520551cf29b1cb9c5efe6ec46
HaithamMahajna <haithammahajna98@gmail.com>
2025-06-28 02:01:55 +0300

    terraform

--- tf/modules/k8s-cluster/autoscaling.tf
+++ tf/modules/k8s-cluster/autoscaling.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "worker_asg" {
   desired_capacity    = 0
   max_size            = 10
   min_size            = 0
-  vpc_zone_identifier = module.polybot_service_vpc.private_subnets
+  vpc_zone_identifier = module.polybot_service_vpc.public_subnets
 
   launch_template {
     id      = aws_launch_template.worker_lt.id
